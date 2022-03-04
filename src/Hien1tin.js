c17ab0454819c66075a0c9ddc9fefe399f0e0d75
TamNguyen372 <tamudpm@gmail.com>
2022-03-04 13:58:26 +0700

    demo react

---
+++ src/Hien1tin.js
@@ -0,0 +1,17 @@
+import './Hien1tin.css';
+
+function Hien1tin() { //const Hien1tin = () =>
+    const t = <
+        div className = 'tin' >
+        <
+        h4 className = "tieude" >
+        <
+        a href = '#' > Tieu de tin < /a>
+    So lan xem < span > 0 < /span></h
+    4 >
+        <
+        p className = 'mota' > Mo ta tin < /p> <
+        /div>
+    return t;
+}
+export default Hien1tin;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Tin from './Hien1tin';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+class Tacgia extends React.Component {
+        render() {
+            return <div > Phat trien boi Nguyen tuong Tam < /div>
+        }
+    }
+    //Vi du
+const Hienngay = () => {
+    const d = new Date();
+    let ngay = d.getDate();
+    let thang = d.getMonth() + 1;
+    let nam = d.getFullYear();
+    return "Hom nay la: " + ngay + "/" + thang + "/" + nam;
+}
+ReactDOM.render( < Tin / > , document.getElementById('root'));
+ReactDOM.render( < Hienngay / > , document.getElementById('root'));
+ReactDOM.render( <
+    React.StrictMode >
+    <
+    App / >
+    <
+    Tacgia / >
+    <
+    /React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
